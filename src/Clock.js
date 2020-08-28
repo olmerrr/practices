8a271889b504ab532042278db733e1a8496f61c1
Yaroslav <dombrovskiy91@gmail.com>
2020-08-28 17:18:31 +0300

    component Clock

---
+++ src/Clock.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+
+export default class Clock extends Component {
+    state = {
+        date: new Date(),
+    }
+
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick(){
+        this.setState({
+            date: new Date()
+        });
+    }
+    render() {
+        return (
+            <div>
+                <p>Точное время: {this.state.date.toLocaleTimeString()}</p>
+    </div>
+    );
+    }
+}
+
