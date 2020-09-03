50e8461d1aaa6f9bbd9cdd9d78f904947c940458
Yaroslav <dombrovskiy91@gmail.com>
2020-09-03 19:20:25 +0300

    setState() addItem component

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import SubscribeForm from './SubscribeForm';
 import Toggle from "./Toggle";
 import ShowWarning from "./ShowWarning";
 import TodoList from "./TodoList";
+import ItemAddForm from "./components/ItemAddForm/index";
 
 export default class App extends Component {
-
+    maxId = 100;
     state = {
         todoData: [
             {label: 'Drink Coffee', important: false, id: 1},
@@ -20,14 +21,30 @@ export default class App extends Component {
         this.setState(({todoData}) => {
             const idx = todoData.findIndex((el) => el.id === id);
             const newArray = [
-                ...todoData.slice(0, idx),
-                ...todoData.slice(idx + 1)
+                ...todoData.slice(0, idx), //выреж от нуля до idx
+                ...todoData.slice(idx + 1) // выреж после idx с шагом на 1
             ]
             return {
                 todoData: newArray
             };
         });
     };
+    addItem = (text) => {
+        const newItem = {
+            label: text,
+            important: false,
+            id: this.maxId++
+        }
+        this.setState(({todoData}) => {
+          const newArr = [
+              ...todoData,
+              newItem
+          ];
+          return {
+              todoData: newArr
+          };
+        })
+    }
 
     render() {
         return (
@@ -42,6 +59,9 @@ export default class App extends Component {
                     <TodoList
                         onDeleted={this.deleteItem}
                         todos={this.state.todoData}/>
+                    <ItemAddForm
+                        onItemAdded={this.addItem}
+                    />
                 </main>
             </div>
         )
