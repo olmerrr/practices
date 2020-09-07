35bbbf1d2b0f5c7fba4d965185db2e5cbad3e298
Yaroslav <dombrovskiy91@gmail.com>
2020-09-07 16:41:27 +0300

    create ItemAddForm input + btn on Form

--- src/components/ItemAddForm/ItemAddForm.jsx
+++ src/components/ItemAddForm/ItemAddForm.jsx
@@ -1,14 +1,38 @@
-import React,{Component} from "react";
+import React, {Component} from "react";
 import "./ItemAddForm.css";
+
 export default class ItemAddForm extends Component {
+    state = {
+        label: ''
+    };
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    }
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        this.props.onItemAdded(this.state.label)
+    }
+
     render() {
-        const {onItemAdded} = this.props;
+
         return (
-            <div className="item-add-form">
+            <form className="item-add-form"
+                  onSubmit={this.onSubmitForm}
+            >
+
+                <input type="text"
+                       placeholder="What i need do?"
+                       className="form-control"
+                       onChange={this.onLabelChange}
+                />
+
                 <button className="btn btn-outline-secondary"
-                        onClick={() => onItemAdded('Hello Yaroslav')}
-                >Add</button>
-            </div>
+                        >
+                    Add
+                </button>
+            </form>
         )
     }
 }
